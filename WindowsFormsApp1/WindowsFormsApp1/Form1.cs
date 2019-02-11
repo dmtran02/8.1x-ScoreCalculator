965af2d39bfe51d1b75e65d4d2b44975d1620be7
dmtran02 <dmtran02@gmail.com>
2019-02-11 11:12:38 -0500

    one change required

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,11 +12,12 @@ using System.Windows.Forms;
  * Duc Tran
  * Professor Frank Friedman
  * CIS 3309 - 8.1X: Score Calculator
- * Last Updated: February 10, 2019
+ * Last Updated: February 11, 2019
  */
 
     /*
-     * !! FINISH THE LAST PART OF THIS LAB!!
+     * !! FINISH THE LAST PART OF THIS LAB !!
+     *  --Remove the score count increments for we now have a list of integers (#12)
      */
 
 namespace WindowsFormsApp1
@@ -26,44 +27,51 @@ namespace WindowsFormsApp1
 
         private static int scoreTotal = 0;
         private static int scoreCount = 0;
-        private static int[] myData = new int[20];
+        private static List<int> myData = new List<int>();
         private static int counter = 0; // for debugging scoreCount
 
         public Form1()
         {
             InitializeComponent();
             this.ActiveControl = txtScore;
-            Console.WriteLine("myData Size: " + myData.Length);
+            //Console.WriteLine("myData Size: " + myData.Length);
         }
 
         private void btnAdd_Click(object sender, EventArgs e) // fix score total global to hold txtscoretotal.text
         {
-            scoreTotal = int.Parse(txtScore.Text);
-            if (!string.IsNullOrEmpty(txtScoreTotal.Text))
+
+            if (!int.TryParse(txtScore.Text, out int n))
             {
-                int scoreHolder = int.Parse(txtScoreTotal.Text);
-                myData[scoreCount] = int.Parse(txtScore.Text);
-                scoreHolder += scoreTotal;
-                txtScoreTotal.Text = scoreHolder.ToString();
-                scoreCount++;
-                txtScoreCount.Text = scoreCount.ToString();
-                //Console.WriteLine("Total Score: " + int.Parse(txtScoreTotal.Text));
-                //Console.WriteLine("Score Count: " + int.Parse(txtScoreCount.Text));
-                int average = int.Parse(txtScoreTotal.Text)/int.Parse(txtScoreCount.Text);
-                //Console.WriteLine("Average: " + average);
-                txtAvg.Text = average.ToString();
-                //Console.WriteLine("First element of myData: " + myData[0]);
+                MessageBox.Show("Please enter a valid integer value in the text box.");
             }
             else
             {
-                txtScoreTotal.Text = scoreTotal.ToString();
-                myData[scoreCount] = int.Parse(txtScore.Text);
-                scoreCount++;
-                txtScoreCount.Text = scoreCount.ToString();
-                txtAvg.Text = (int.Parse(txtScoreTotal.Text)/int.Parse(txtScoreCount.Text)).ToString();
-                //Console.WriteLine("First element of myData: " + myData[0]);
+                scoreTotal = int.Parse(txtScore.Text);
+                if (!string.IsNullOrEmpty(txtScoreTotal.Text))
+                {
+                    int scoreHolder = int.Parse(txtScoreTotal.Text);
+                    myData.Add(int.Parse(txtScore.Text));
+                    scoreHolder += scoreTotal;
+                    txtScoreTotal.Text = scoreHolder.ToString();
+                    scoreCount++;
+                    txtScoreCount.Text = scoreCount.ToString();
+                    //Console.WriteLine("Total Score: " + int.Parse(txtScoreTotal.Text));
+                    //Console.WriteLine("Score Count: " + int.Parse(txtScoreCount.Text));
+                    int average = int.Parse(txtScoreTotal.Text) / int.Parse(txtScoreCount.Text);
+                    //Console.WriteLine("Average: " + average);
+                    txtAvg.Text = average.ToString();
+                    //Console.WriteLine("First element of myData: " + myData[0]);
+                }
+                else
+                {
+                    txtScoreTotal.Text = scoreTotal.ToString();
+                    myData.Add(int.Parse(txtScore.Text));
+                    scoreCount++;
+                    txtScoreCount.Text = scoreCount.ToString();
+                    txtAvg.Text = (int.Parse(txtScoreTotal.Text) / int.Parse(txtScoreCount.Text)).ToString();
+                    //Console.WriteLine("First element of myData: " + myData[0]);
+                }
             }
-            //txtScore.Focus();
             
         }
 
@@ -97,32 +105,33 @@ namespace WindowsFormsApp1
             txtScoreCount.Text = "";
             txtScore.Text = "";
             txtAvg.Text = "";
-            myData = new int[20];
+            myData = new List<int>();
             txtScore.Focus();
         }
 
         private void btnDisplayScores_Click(object sender, EventArgs e)
         {
             String numDisplay = "";
+            myData.Sort();
             int counter = 0;
+            bool isEmpty = !myData.Any();
+            if (isEmpty)
+            {
+                MessageBox.Show("Enter scores into the form to begin!");
+            }
             foreach (int i in myData)
             {
                 numDisplay += i.ToString() + "\n";
                 //MessageBox.Show(numDisplay, "Sorted Scores");
                 counter++;
-                if(scoreCount == 0)
-                {
-                    MessageBox.Show("Enter scores into the form to begin!");
-                    break;
-                }
-                else if(counter >= 20)
+                if (counter >= 20)
                 {
                     MessageBox.Show("This example will only display the first 20 scores!");
                     break;
                 }
                 else if(counter == scoreCount)
                 {
-                    MessageBox.Show(numDisplay, "Sorted Scores");
+                    MessageBox.Show(numDisplay, "Sorted Scores: " + counter);
                     break;
                 }
             }
