5127f33471147602f08349a65a8eea85c88a49e5
dmtran02 <dmtran02@gmail.com>
2019-02-11 22:07:49 -0500

    finalized project

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -15,29 +15,35 @@ using System.Windows.Forms;
  * Last Updated: February 11, 2019
  */
 
-    /*
-     * !! FINISH THE LAST PART OF THIS LAB !!
-     *  --Remove the score count increments for we now have a list of integers (#12)
-     */
-
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
 
+        /*
+         * Class variables for the sake of operation of program running as a success
+         */
+
         private static int scoreTotal = 0;
-        private static int scoreCount = 0;
+        //private static int scoreCount = 0;
         private static List<int> myData = new List<int>();
-        private static int counter = 0; // for debugging scoreCount
+        //private static int counter = 0; // for debugging scoreCount
 
         public Form1()
         {
             InitializeComponent();
             this.ActiveControl = txtScore;
-            //Console.WriteLine("myData Size: " + myData.Length);
         }
 
-        private void btnAdd_Click(object sender, EventArgs e) // fix score total global to hold txtscoretotal.text
+        /*
+         * If unable to parse score, message box will notify user to enter a valid 'numeric' score
+         * Otherwise, score is parsed successfully and is added onto 'myData' list and the count is tracked by how many elements are in myData list.
+         * 
+         * The text of txtScoreCount is set to the count of myData and average is the division of parsed 'scoreTotal' and 'scoreCount' which is then set as the text of txtAvg.
+         *  -There are if statements depending if there was a value inputted before or not to provide appropriate display, functionality and calculation.
+         */
+
+        private void btnAdd_Click(object sender, EventArgs e)
         {
 
             if (!int.TryParse(txtScore.Text, out int n))
@@ -53,8 +59,8 @@ namespace WindowsFormsApp1
                     myData.Add(int.Parse(txtScore.Text));
                     scoreHolder += scoreTotal;
                     txtScoreTotal.Text = scoreHolder.ToString();
-                    scoreCount++;
-                    txtScoreCount.Text = scoreCount.ToString();
+                    //scoreCount++;
+                    txtScoreCount.Text = myData.Count.ToString();
                     //Console.WriteLine("Total Score: " + int.Parse(txtScoreTotal.Text));
                     //Console.WriteLine("Score Count: " + int.Parse(txtScoreCount.Text));
                     int average = int.Parse(txtScoreTotal.Text) / int.Parse(txtScoreCount.Text);
@@ -66,8 +72,8 @@ namespace WindowsFormsApp1
                 {
                     txtScoreTotal.Text = scoreTotal.ToString();
                     myData.Add(int.Parse(txtScore.Text));
-                    scoreCount++;
-                    txtScoreCount.Text = scoreCount.ToString();
+                    //scoreCount++;
+                    txtScoreCount.Text = myData.Count.ToString();
                     txtAvg.Text = (int.Parse(txtScoreTotal.Text) / int.Parse(txtScoreCount.Text)).ToString();
                     //Console.WriteLine("First element of myData: " + myData[0]);
                 }
@@ -75,10 +81,18 @@ namespace WindowsFormsApp1
             
         }
 
+        /*
+         * Closes the program upon button click
+         */
+
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
+        /*
+         * Event handler for btnAdd_Click event handler that adds value in txtScore if the 'Enter' key is pressed while highlighting the add button on the form.
+         */
 
         private void EnterPress(object sender, KeyPressEventArgs e)
         {
@@ -88,6 +102,10 @@ namespace WindowsFormsApp1
             }
         }
 
+        /*
+         * Event handler for btnExit_Click event handler that adds value in txtScore if the 'Escape' (or ESC) key is pressed while highlighting the exit button on the form.
+         */
+
         private void EscPress(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == (char)Keys.Escape)
@@ -96,10 +114,16 @@ namespace WindowsFormsApp1
             }
         }
 
+        /*
+         * Event hanlder that takes care of resetting every textbox, 
+         * list and class variable values to default and sets focus onto 
+         * txtScore for a new series of inputs from the user.
+         */
+
         private void btnClrScores_Click(object sender, EventArgs e)
         {
             scoreTotal = 0;
-            scoreCount = 0;
+            //scoreCount = 0;
             txtScore.Text = "";
             txtScoreTotal.Text = "";
             txtScoreCount.Text = "";
@@ -108,6 +132,19 @@ namespace WindowsFormsApp1
             myData = new List<int>();
             txtScore.Focus();
         }
+
+        /*
+         * Event handler for 'Display Scores' button that sorts myData list and displays 
+         * in a message box the ordered elements of the list (least to greatest) 
+         * and the count of elements in the list.
+         * 
+         * First checks if the list is empty:
+         *  -If empty, the form will notify the user to enter scores in order for this button to display scores that have been entered
+         *  -Otherwise, the form will display list the first 20 scores (and only 20) along with its count or notify the user that
+         *   only the first 20 scores will be displayed if there are more than 20 scores inputted by the user in a message box.
+         *   
+         *  At the end of this method, txtScore is focused for the convenience of future inputs from the user.
+         */
 
         private void btnDisplayScores_Click(object sender, EventArgs e)
         {
@@ -124,14 +161,14 @@ namespace WindowsFormsApp1
                 numDisplay += i.ToString() + "\n";
                 //MessageBox.Show(numDisplay, "Sorted Scores");
                 counter++;
-                if (counter >= 20)
+                if (myData.Count >= 20)
                 {
                     MessageBox.Show("This example will only display the first 20 scores!");
                     break;
                 }
-                else if(counter == scoreCount)
+                else if(counter == myData.Count)
                 {
-                    MessageBox.Show(numDisplay, "Sorted Scores: " + counter);
+                    MessageBox.Show(numDisplay, "Sorted Scores: " + myData.Count);
                     break;
                 }
             }
